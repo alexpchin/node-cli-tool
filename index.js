9dc315f69e3736993cff1da83b626f05661f3bb6
alexpchin <alex@alexchin.co.uk>
2015-09-30 00:00:19 +0100

    Adding some more comments

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var userArgs = process.argv.slice(2);
 var searchPattern = userArgs[0];
 
 var exec = require('child_process').exec;
+
+// To run a command in the system we can use the exec method of the child_process module - a module that comes with Node and doesn't need to be separately installed
 var child = exec('ls -a | grep ' + searchPattern, function(err, stdout, stderr) {
   console.log(stdout);
 });
